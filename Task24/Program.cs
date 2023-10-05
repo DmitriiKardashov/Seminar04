c07857fc2f48368c702a54083aaeea5c54754168
DimiyanKo <karddmitrij@yandex.ru>
2023-10-05 13:02:28 +0300

    Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int SumNumbers(int num)
+{
+    int sum = 0;
+    for(int i = 1; i <= num; i++)
+    checked
+    {
+        sum = sum + i;
+    }
+    return sum;   
+}
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number>0)
+{
+    int sumNumbers = SumNumbers(number);
+    Console.WriteLine($"{number} -> {sumNumbers}");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
