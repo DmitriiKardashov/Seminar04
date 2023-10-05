cdb32b7c9437d8dcbe67cb6234be84e3e400e8b9
DimiyanKo <karddmitrij@yandex.ru>
2023-10-05 17:45:00 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+
+int DegOfNumbers(int x, int y)
+{
+    int z = 1;
+    if (y >0)
+    {
+        for (int i = 1; i <= y; i++)
+        {
+         z = z*x;
+        }
+     return z;
+    }
+    else return 0;
+}
+
+Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое положительное число");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int res = DegOfNumbers(A, B);
+Console.WriteLine($"{A} в степени {B} равно {res}");
