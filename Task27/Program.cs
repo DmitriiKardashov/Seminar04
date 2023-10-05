4338c789eb029b87454b91b933c1a51ae83420a1
DimiyanKo <karddmitrij@yandex.ru>
2023-10-05 18:21:27 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход 
+число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int CountDigit(int num)
+{
+    if (num < 0) num = num*-1;
+    if (num/10 < 0) return num;
+    int res = 0;
+    while (num > 0)
+    {
+        res = res + num%10;
+        num = num/10;
+    }
+    return res;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int countDigit = CountDigit(number);
+Console.WriteLine($"{number} -> {countDigit}");
