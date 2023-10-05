b4a468f215c6e34642ce9ddaab9af0238f1818e8
DimiyanKo <karddmitrij@yandex.ru>
2023-10-05 13:42:20 +0300

    Task28

---
+++ Task28/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int Factorial(int num)
+{
+    int sum = 1;
+    for(int i = 1; i <= num; i++)
+    checked
+    {
+        sum = sum * i;
+    }
+    return sum;   
+}
+
+Console.WriteLine("Введите целое положительное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number>0)
+{
+    int factorial = Factorial(number);
+    Console.WriteLine($"{number} -> {factorial}");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
+}
