320f3dda20e92838803f2da3db69f4bcba119667
DimiyanKo <karddmitrij@yandex.ru>
2023-10-05 13:29:26 +0300

    Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int CountDigit(int num)
+{
+    if (num == 0) return 1;
+    int count = 0;
+    while (num != 0)
+    {
+        num = num/10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int countDigit = CountDigit(number);
+Console.WriteLine($"{number} -> {countDigit}");
